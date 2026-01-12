4d30f9071eb5844de90ca827a8394f33fde9f353
Shubham Bhandari <shubh.gehu@gmail.com>
2026-01-12 13:57:09 +0530

    removed unncecessary files

--- core/models/deduct_bill_distribution.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-
-
-class DeductBillDistributionEntry(BaseModel):
-    CustomerNum: str = Field(..., min_length=1)
-    EMailAddress: EmailStr
-
-    class Config:
-        extra = "allow"
