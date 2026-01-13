e55844112445adc6b3deea5b398bf03fb0ee8ad8
Shubham Bhandari <83971506+shubhambhandari29@users.noreply.github.com>
2026-01-13 14:02:34 +0530

    5

--- core/models/hcm_users.py
+++ core/models/hcm_users.py
@@ -1,16 +1,11 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr
 
 
 class HCMUserUpsert(BaseModel):
-    PK_Number: int | None = None
-    UserTitle: str | None = None
-    UserID: str | None = None
-    UserName: str | None = None
-    UserEmail: EmailStr | None = None
-    TelNum: str | None = None
-    TelExt: int | None = None
+    UserTitle: str | None = None 
+    UserName: str | None = None 
+    UserEmail: EmailStr | None = None  
     UserAction: str | None = None
-    CustomerNum: str | None = None
-    LanID: str | None = None
-    PROD_Password: str | None = None
-    UAT_Password: str | None = None
+
+    class Config:
+        extra="allow"
