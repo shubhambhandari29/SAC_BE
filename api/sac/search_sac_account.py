047f5acb00080a52e6825a72b7f1e439a5dfc65f
Shubham Bhandari <83971506+shubhambhandari29@users.noreply.github.com>
2026-01-13 14:00:36 +0530

    3

--- api/sac/search_sac_account.py
+++ api/sac/search_sac_account.py
@@ -11,5 +11,3 @@ router = APIRouter(dependencies=[Depends(get_current_user_from_token)])
 @router.get("/")
 async def get_sac_account_records(search_by: str = Query(..., alias="search_by")):
     return await get_sac_account_records_service(search_by)
-
-
